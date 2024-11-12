73f78359b87512227070411f37d76e050bcbb939
ValerieMbaka <valeriembaka0@gmail.com>
2024-11-12 16:31:52 +0300

    updated models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+        prod_name = models.CharField(max_length=100)
+        prod_price = models.IntegerField()
+        prod_category = models.CharField(max_length=50)
+        prod_quantity = models.IntegerField()
+        prod_description = models.TextField()
+        prod_image = models.ImageField()
+
