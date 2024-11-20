52c677b7841c228fb1f22adb7667805b8526cf15
ValerieMbaka <valeriembaka0@gmail.com>
2024-11-21 00:03:05 +0300

    updated admin

--- products/models.py
+++ products/models.py
@@ -11,3 +11,24 @@ class Product(models.Model):
 
         def __str__(self):
                 return self.prod_name
+
+class Author(models.Model):
+        name = models.CharField(max_length=100, blank=False)
+
+        def __str__(self):
+                return self.name
+
+class Book(models.Model):
+        title = models.CharField(max_length=100, blank=False)
+        author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+        def __str__(self):
+                return self.title
+
+class Library(models.Model):
+        name = models.CharField(max_length=100, blank=False)
+        book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+        def __str__(self):
+                return self.name
+# make and run migrations after creating and registering the models
